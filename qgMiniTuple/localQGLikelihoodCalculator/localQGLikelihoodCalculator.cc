38f4a09d21c14cef02a485059bd3bae3a2d8ae61
UAEDF-tomc <tom.cornelis@ua.ac.be>
2014-10-10 12:09:16 +0200

    last commit was not needed

--- qgMiniTuple/localQGLikelihoodCalculator/localQGLikelihoodCalculator.cc
+++ qgMiniTuple/localQGLikelihoodCalculator/localQGLikelihoodCalculator.cc
@@ -50,7 +50,6 @@ bool QGLikelihoodCalculator::init(const TString& fileName){
 
 /// Compute the QGLikelihood, given the pT, eta, rho and likelihood variables vector
 float QGLikelihoodCalculator::computeQGLikelihood(float pt, float eta, float rho, std::vector<float> vars){
-  eta = fabs(eta);
   if(!isValidRange(pt, rho, eta)) return -1;
 
   float Q=1., G=1.;

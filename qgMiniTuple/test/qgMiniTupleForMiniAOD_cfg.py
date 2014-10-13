eff134c6ced90a554d7978ad56e4b5b108e4d391
UAEDF-tomc <tom.cornelis@ua.ac.be>
2014-10-13 12:51:28 +0200

    Run on local test file, remove global tag part

--- qgMiniTuple/test/qgMiniTupleForMiniAOD_cfg.py
+++ qgMiniTuple/test/qgMiniTupleForMiniAOD_cfg.py
@@ -5,15 +5,13 @@ process = cms.Process("qgMiniTupleProducer")
 # Settings for local tests
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
 process.source = cms.Source("PoolSource", 
-    fileNames = cms.untracked.vstring('/store/mc/Spring14miniaod/QCD_Pt-30to50_Tune4C_13TeV_pythia8/MINIAODSIM/castor_PU20bx25_POSTLS170_V5-v1/00000/02EF5246-7F06-E411-A4A2-002590596486.root') #Test file available at T2B.
+    fileNames = cms.untracked.vstring('file:/user/tomc/public/TTJets_forSynch_1.root')
 )
 
 # Standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'START71_V8A::All'
 
 # Use TFileService to put trees from different analyzers in one file
 process.TFileService = cms.Service("TFileService", 

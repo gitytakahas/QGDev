4cf8adfa4fc4bdabf1c097e0e99afdac8ba66d69
UAEDF-tomc <tom.cornelis@ua.ac.be>
2014-11-24 16:51:42 +0100

    Update ROC plots with decorrelated and var-weighted likelihood

--- qgMiniTuple/macros/plotROC.C
+++ qgMiniTuple/macros/plotROC.C
@@ -25,17 +25,16 @@ int main(int argc, char**argv){
 
   // Define binning for pdfs
   binClass bins;
-  bins.setBinRange("eta", {0,1.3,1.5,2,2.5,3,4.7});
-  bins.setBinRange("pt" , bins.getBins(20, 20, 2000, true, {6500}));				// i.e. 20 bins from 20 to 2000 with log=true and with an additional bin up to 6500
-  bins.setBinRange("rho", {0, 9999});
+  bins.setBinRange("eta", 	"#eta", 	{0,1.3,1.5,2,2.5,3,4.7});
+  bins.setBinRange("pt" , 	"p_{T}", 	bins.getBins(20, 20, 2000, true, {6500}));				// i.e. 20 bins from 20 to 2000 with log=true and with an additional bin up to 6500
   bins.printBinRanges();
 
   // Link some bins to be merged because of low statistics (for example higher pT bins at large eta)
-  for(int i=10; i < 21; ++i) bins.setLinks("eta5_pt9_rho0", {TString::Format("eta5_pt%d_rho0",i)});
-  for(int i=14; i < 21; ++i) bins.setLinks("eta4_pt13_rho0", {TString::Format("eta4_pt%d_rho0",i)});
-  for(int i=16; i < 21; ++i) bins.setLinks("eta3_pt15_rho0", {TString::Format("eta3_pt%d_rho0",i)});
-  for(int i=18; i < 21; ++i) bins.setLinks("eta2_pt17_rho0", {TString::Format("eta2_pt%d_rho0",i)});
-  for(int i=19; i < 21; ++i) bins.setLinks("eta1_pt18_rho0", {TString::Format("eta1_pt%d_rho0",i)});
+  for(int i=10; i < 21; ++i) bins.setLinks("eta5_pt9", {TString::Format("eta5_pt%d",i)});
+  for(int i=14; i < 21; ++i) bins.setLinks("eta4_pt13", {TString::Format("eta4_pt%d",i)});
+  for(int i=16; i < 21; ++i) bins.setLinks("eta3_pt15", {TString::Format("eta3_pt%d",i)});
+  for(int i=18; i < 21; ++i) bins.setLinks("eta2_pt17", {TString::Format("eta2_pt%d",i)});
+  for(int i=19; i < 21; ++i) bins.setLinks("eta1_pt18", {TString::Format("eta1_pt%d",i)});
 
   // Loop over different samples and jet types
   for(TString file : files){
@@ -44,15 +43,16 @@ int main(int argc, char**argv){
       system("rm -rf plots/ROC/" + file + "/" + jetType);
 
       treeLooper t(file, jetType);						// Init tree
-      bins.setReference("rho", &t.rho);
       bins.setReference("pt",  &t.pt);
       bins.setReference("eta", &t.eta);
 
       // Init local QGLikelihoodCalculator
-      QGLikelihoodCalculator localQG("../data/pdfQG_" + jetType + "_13TeV.root");
-      QGLikelihoodCalculator localQG_cdf("../data/pdfQG_" + jetType + "_fineBinning_13TeV.root");
-      QGLikelihoodCalculator localQG2("../data/pdfQG_" + jetType + "_13TeV_testVarTransform.root", true);
-      QGLikelihoodCalculator localQG2_cdf("../data/pdfQG_" + jetType + "_fineBinning_13TeV_testVarTransform.root", true);
+      QGLikelihoodCalculator localQG("../data/pdfQG_" + jetType + "_13TeV_testWithVarTransform.root");
+      QGLikelihoodCalculator localQG_cdf("../data/pdfQG_" + jetType + "_fineBinning_13TeV_testWithVarTransform.root");
+      QGLikelihoodCalculator localQG2("../data/pdfQG_" + jetType + "_13TeV_testWithVarTransform.root", true);
+      QGLikelihoodCalculator localQG2_cdf("../data/pdfQG_" + jetType + "_fineBinning_13TeV_testWithVarTransform.root", true);
+      QGLikelihoodCalculator localQG3("../data/pdfQG_" + jetType + "_13TeV_testWithVarTransform.root", false, true);
+      QGLikelihoodCalculator localQG3_cdf("../data/pdfQG_" + jetType + "_fineBinning_13TeV_testWithVarTransform.root", false, true);
 
       // Creation of histos
       std::map<TString, TH1D*> plots;
@@ -62,7 +62,7 @@ int main(int argc, char**argv){
           plots["axis2"  + histName] 	= new TH1D("axis2"  + histName, "axis2"     + histName, 1000, 0, 8);
           plots["ptD"    + histName]	= new TH1D("ptD"    + histName, "ptD"       + histName, 1000, 0, 1);
           plots["mult"   + histName]	= new TH1D("mult"   + histName, "mult"      + histName, 100, 0.5, 100.5);
-          for(TString var : {"qg","qg2","axis2","ptD","mult"}){
+          for(TString var : {"qg","qg2","qg3","axis2","ptD","mult"}){
             for(TString type : {"_l","_c"}) plots[var + type + histName] = new TH1D(var + type + histName, var + type + histName, 1000, -0.0001, 1.0001);
           }
         }
@@ -87,16 +87,22 @@ int main(int argc, char**argv){
         plots["axis2"   + histName]->Fill(t.axis2, 											t.weight);
         plots["ptD"     + histName]->Fill(t.ptD, 											t.weight);
         plots["mult"    + histName]->Fill(t.mult, 											t.weight);
-        plots["qg_l"    + histName]->Fill(localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {(float) t.mult, t.ptD, t.axis2}),	t.weight);
-        plots["qg2_l"    + histName]->Fill(localQG2.computeQGLikelihood(     t.pt, t.eta, t.rho, {t.axis2, (float) t.mult, t.ptD}),	t.weight);
-        plots["axis2_l" + histName]->Fill(localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {-1, -1, t.axis2}), 			t.weight);
-        plots["ptD_l"   + histName]->Fill(localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {-1, t.ptD}), 				t.weight);
-        plots["mult_l"  + histName]->Fill(localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {(float) t.mult}), 			t.weight);
-        plots["qg_c"    + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {(float) t.mult, t.ptD, t.axis2}), 	t.weight);
-        plots["qg2_c"   + histName]->Fill(localQG2_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {t.axis2, (float) t.mult, t.ptD}), 	t.weight);
-        plots["axis2_c" + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {-1, -1, t.axis2}), 			t.weight);
-        plots["ptD_c"   + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {-1, t.ptD}), 				t.weight);
-        plots["mult_c"  + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {(float) t.mult}), 			t.weight);
+        plots["qg_l"    + histName]->Fill(localQG.computeQGLikelihood(        t.pt, t.eta, t.rho, {(float) t.mult, t.ptD, t.axis2}),	t.weight);
+//      plots["qg2_l"   + histName]->Fill(localQG.computeQGLikelihood(        t.pt, t.eta, t.rho, {(float) t.mult, -1, t.axis2}),	t.weight);
+        plots["qg2_l"   + histName]->Fill(localQG3.computeQGLikelihood(       t.pt, t.eta, t.rho, {(float) t.mult, t.ptD, t.axis2}),	t.weight);
+        plots["qg3_l"   + histName]->Fill(localQG2.computeQGLikelihood(       t.pt, t.eta, t.rho, {t.axis2, t.ptD, (float) t.mult}),	t.weight);	// Decorrelation takes other order
+//        plots["qg4_l"   + histName]->Fill(localQG2.computeQGLikelihood(       t.pt, t.eta, t.rho, {t.axis2, (float) t.mult}),		t.weight);
+        plots["axis2_l" + histName]->Fill(localQG.computeQGLikelihood(        t.pt, t.eta, t.rho, {-1, -1, t.axis2}), 			t.weight);
+        plots["ptD_l"   + histName]->Fill(localQG.computeQGLikelihood(        t.pt, t.eta, t.rho, {-1, t.ptD}), 			t.weight);
+        plots["mult_l"  + histName]->Fill(localQG.computeQGLikelihood(        t.pt, t.eta, t.rho, {(float) t.mult}), 			t.weight);
+        plots["qg_c"    + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF( t.pt, t.eta, t.rho, {(float) t.mult, t.ptD, t.axis2}), 	t.weight);
+//      plots["qg2_c"   + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF( t.pt, t.eta, t.rho, {(float) t.mult, -1, t.axis2}), 	t.weight);
+        plots["qg2_c"   + histName]->Fill(localQG3_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {(float) t.mult, t.ptD, t.axis2}), 	t.weight);
+        plots["qg3_c"   + histName]->Fill(localQG2_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {t.axis2, t.ptD, (float) t.mult}), 	t.weight);
+//        plots["qg4_c"   + histName]->Fill(localQG2_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {t.axis2, (float) t.mult}), 	t.weight);
+        plots["axis2_c" + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF( t.pt, t.eta, t.rho, {-1, -1, t.axis2}), 			t.weight);
+        plots["ptD_c"   + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF( t.pt, t.eta, t.rho, {-1, t.ptD}), 			t.weight);
+        plots["mult_c"  + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF( t.pt, t.eta, t.rho, {(float) t.mult}), 			t.weight);
       }
       for(auto& plot : plots) plot.second->Scale(1./plot.second->Integral(0, plot.second->GetNbinsX() + 1));
 
@@ -110,7 +116,7 @@ int main(int argc, char**argv){
         l.SetBorderSize(0);
 
         std::map<TString, TGraph*> roc;
-        for(TString var : {"qg","qg2","axis2","ptD","mult"}){
+        for(TString var : {"qg","qg2","qg3","axis2","ptD","mult"}){
           for(TString type : {"_l","_c",""}){
             if(var.Contains("qg") && type == "") continue;
 
@@ -125,11 +131,13 @@ int main(int argc, char**argv){
               roc[var+type]->SetPoint(bin, gluonRej, quarkEff);
             }
 
-            if(type == "_l")  	 	roc[var+type]->SetLineColor(var == "qg2" ? 30 : (var == "qg"? kGray+1 : (var == "axis2"? kGreen+4 : (var == "ptD"? kMagenta+4 : kRed))));
-            else if(type == "_c")  	roc[var+type]->SetLineColor(var == "qg2" ? 3 : (var == "qg"? kBlack : (var == "axis2"? kYellow : (var == "ptD"? kAzure+10 : kOrange))));
+            if(type == "_l")  	 	roc[var+type]->SetLineColor(var == "qg4" ? 42 : (var == "qg3" ? 46 : (var == "qg2" ? 30 : (var == "qg"? kGray+1 : (var == "axis2"? kGreen+4 : (var == "ptD"? kMagenta+4 : kRed))))));
+            else if(type == "_c")  	roc[var+type]->SetLineColor(var == "qg4" ? 28 : (var == "qg3" ? 39 : (var == "qg2" ? 3 : (var == "qg"? kBlack : (var == "axis2"? kYellow : (var == "ptD"? kAzure+10 : kOrange))))));
             else		 	roc[var+type]->SetLineColor(var == "axis2"? kGreen+4 : (var == "ptD"? kMagenta+4 : kRed));
-	    roc[var+type]->SetLineWidth(type == "_l" ? 3 : 1);
+//	    roc[var+type]->SetLineWidth(type == "_l" ? 3 : 1);
+	    roc[var+type]->SetLineWidth(type == "_l" ? 1.5 : 0.5);
             roc[var+type]->SetLineStyle(type == "" ? 3 : 1);
+
 
             TString entryName = "quark-gluon";
             if(var == "axis2") 	entryName = "-log(#sigma_{2})";
@@ -137,10 +145,11 @@ int main(int argc, char**argv){
             if(var == "mult") 	entryName = "multiplicity";
             if(type == "_l")	entryName += " likelihood";
             if(type == "_c")	entryName += " CDF-likelihood";
-            if(var == "qg2") 	entryName += " (uncorrelated)";
+            if(var == "qg2") 	entryName += " (var weighted)";
+            if(var == "qg3") 	entryName += " (uncorrelated)";
             l.AddEntry(roc[var+type], entryName, "l");
 
-            if(var+type == "qg_l"){
+            if(roc.size() == 1){
               roc[var+type]->GetYaxis()->SetTitle("quark-jet efficiency");
               roc[var+type]->GetXaxis()->SetTitle("gluon-jet rejection");
               roc[var+type]->GetXaxis()->SetRangeUser(0,1);
@@ -153,15 +162,7 @@ int main(int argc, char**argv){
         l.Draw();
         c.Modified();
 
-        TLatex t;
-        t.SetNDC(kTRUE);
-        t.SetTextAlign(33);
-        t.SetTextSize(0.02);
-        t.DrawLatex(0.9,0.98,  TString::Format("%.1f < #rho < %.1f", bins.getLowerEdge(plot.first, "rho"), bins.getUpperEdge(plot.first, "rho")));
-        t.DrawLatex(0.9,0.955, TString::Format("%.1f < #eta < %.1f", bins.getLowerEdge(plot.first, "eta"), bins.getUpperEdge(plot.first, "eta")));
-        t.DrawLatex(0.9,0.93,  TString::Format("%.1f < p_{T} < %.1f", bins.getLowerEdge(plot.first, "pt"), bins.getUpperEdge(plot.first, "pt")));
-        t.SetTextAlign(13);
-        t.DrawLatex(0.1,0.93,  jetType);
+        bins.printInfoOnPlot(plot.first, jetType);
 
         TString pdfDir = "./plots/ROC/" + file + "/" + jetType + "/";
         TString pdfName = pdfDir + plot.first + ".pdf";

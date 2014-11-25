f03f86983118dcbcb4022fc0f7144944b543a991
UAEDF-tomc <tom.cornelis@ua.ac.be>
2014-11-25 16:59:06 +0100

    Add nNeutral

--- qgMiniTuple/macros/plotProfile2D.C
+++ qgMiniTuple/macros/plotProfile2D.C
@@ -14,10 +14,9 @@
 
 
 int main(int argc, char**argv){
-  bool useBins = true;
+ for(bool useBins : {true, false}){
   bool extraBinInformation = false;
 
-//  std::vector<TString> files	= {"QCD_AllPtBins"};
   std::vector<TString> files	= {"QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8_S14"};
   std::vector<TString> jetTypes = {"AK4chs"};
 
@@ -29,7 +28,7 @@ int main(int argc, char**argv){
 
   // Link some bins to be merged because of low statistics (for example higher pT bins at large eta)
   if(useBins){
-    for(int i=10; i < 21; ++i) bins.setLinks("eta5_pt9", {TString::Format("eta5_pt%d",i)});
+    for(int i=10; i < 21; ++i) bins.setLinks("eta5_pt9",  {TString::Format("eta5_pt%d",i)});
     for(int i=14; i < 21; ++i) bins.setLinks("eta4_pt13", {TString::Format("eta4_pt%d",i)});
     for(int i=16; i < 21; ++i) bins.setLinks("eta3_pt15", {TString::Format("eta3_pt%d",i)});
     for(int i=18; i < 21; ++i) bins.setLinks("eta2_pt17", {TString::Format("eta2_pt%d",i)});
@@ -38,11 +37,11 @@ int main(int argc, char**argv){
 
   // Define x and y-axis variable and bins
 //std::vector<float> xAxisBins	= bins.getBins(40, 0, 4, false, {4.2,4.4,4.7});		TString xVar = "#eta";		bool xLog = false;
-//std::vector<float> yAxisBins	= bins.getBins(20, 20, 2000, true, {6500});		TString yVar = "p_{T}"; 	bool yLog = true;
   std::vector<float> xAxisBins	= bins.getBins(8, 0, 40, false, {50});			TString xVar = "#rho";		bool xLog = false;
-//std::vector<float> yAxisBins	= bins.getBins(10, 10, 70, false);			TString yVar = "nPileUp"; 	bool yLog = false;
-  std::vector<float> yAxisBins	= bins.getBins(10, 5, 55, false);			TString yVar = "nPriVtxs"; 	bool yLog = false;
-  TString xAndyVar = "rho_nPriVtxs";
+//std::vector<float> yAxisBins	= bins.getBins(20, 20, 2000, true, {6500});		TString yVar = "p_{T}"; 	bool yLog = true;
+  std::vector<float> yAxisBins	= bins.getBins(10, 10, 70, false);			TString yVar = "nPileUp"; 	bool yLog = false;
+//std::vector<float> yAxisBins	= bins.getBins(10, 5, 55, false);			TString yVar = "nPriVtxs"; 	bool yLog = false;
+  TString xAndyVar = "rho_nPileUp";
 
   // Needed for 3D plots (which will be needed to get kurtosis and skewness)
   std::vector<float> axis2Bins	= bins.getBins(100, 0, 8, false);
@@ -69,8 +68,8 @@ int main(int argc, char**argv){
 //    float& xAxisVar 	= t.eta;
 //    float& yAxisVar 	= t.pt;
       float& xAxisVar 	= t.rho;
-//      int& yAxisVar 	= t.nPileUp;
-      int& yAxisVar 	= t.nPriVtxs;
+      int& yAxisVar 	= t.nPileUp;
+//    int& yAxisVar 	= t.nPriVtxs;
 
       // Init local QGLikelihoodCalculator
       QGLikelihoodCalculator localQG("../data/pdfQG_" + jetType + "_13TeV.root");
@@ -82,19 +81,21 @@ int main(int argc, char**argv){
       for(TString type : {"quark","gluon"}){
         for(TString binName : bins.getAllBinNames()){
           TString histName = "_" + type + "_" + binName;
-          plots["axis2" + histName] = new TProfile2D("axis2" + histName, "mean -log(#sigma_{2})",           xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
-          plots["ptD"   + histName] = new TProfile2D("ptD"   + histName, "mean p_{T}D",                     xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
-          plots["mult"  + histName] = new TProfile2D("mult"  + histName, "mean multiplicity",               xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
-          plots["nChg"  + histName] = new TProfile2D("nChg"  + histName, "mean charged multiplicity",       xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
-          plots["qg"    + histName] = new TProfile2D("qg"    + histName, "mean quark-gluon likelihood",     xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
-          plots["cdf"   + histName] = new TProfile2D("cdf"   + histName, "mean quark-gluon CDF-likelihood", xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
+          plots["axis2"    + histName] = new TProfile2D("axis2"    + histName, "mean -log(#sigma_{2})",           xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
+          plots["ptD"      + histName] = new TProfile2D("ptD"      + histName, "mean p_{T}D",                     xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
+          plots["mult"     + histName] = new TProfile2D("mult"     + histName, "mean multiplicity",               xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
+          plots["nChg"     + histName] = new TProfile2D("nChg"     + histName, "mean charged multiplicity",       xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
+          plots["nNeutral" + histName] = new TProfile2D("nNeutral" + histName, "mean neutral multiplicity",       xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
+          plots["qg"       + histName] = new TProfile2D("qg"       + histName, "mean quark-gluon likelihood",     xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
+          plots["cdf"      + histName] = new TProfile2D("cdf"      + histName, "mean quark-gluon CDF-likelihood", xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray);
           if(extraBinInformation){
-            plots3D["axis2" + histName] = new TH3D("axis2" + histName + "3D", "-log(#sigma_{2})",           xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, axis2Bins.size()-1, axis2BinsArray);
-            plots3D["ptD"   + histName] = new TH3D("ptD"   + histName + "3D", "p_{T}D",                     xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, otherBins.size()-1, otherBinsArray);
-            plots3D["mult"  + histName] = new TH3D("mult"  + histName + "3D", "multiplicity",               xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, multBins.size()-1, multBinsArray);
-            plots3D["nChg"  + histName] = new TH3D("nChg"  + histName + "3D", "charged multiplicity",       xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, multBins.size()-1, multBinsArray);
-            plots3D["qg"    + histName] = new TH3D("qg"    + histName + "3D", "quark-gluon likelihood",     xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, otherBins.size()-1, otherBinsArray);
-            plots3D["cdf"   + histName] = new TH3D("cdf"   + histName + "3D", "quark-gluon CDF-likelihood", xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, otherBins.size()-1, otherBinsArray);
+            plots3D["axis2"    + histName] = new TH3D("axis2"    + histName + "3D", "-log(#sigma_{2})",           xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, axis2Bins.size()-1, axis2BinsArray);
+            plots3D["ptD"      + histName] = new TH3D("ptD"      + histName + "3D", "p_{T}D",                     xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, otherBins.size()-1, otherBinsArray);
+            plots3D["mult"     + histName] = new TH3D("mult"     + histName + "3D", "multiplicity",               xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, multBins.size()-1, multBinsArray);
+            plots3D["nChg"     + histName] = new TH3D("nChg"     + histName + "3D", "charged multiplicity",       xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, multBins.size()-1, multBinsArray);
+            plots3D["nNeutral" + histName] = new TH3D("nNeutral" + histName + "3D", "neutral multiplicity",       xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, multBins.size()-1, multBinsArray);
+            plots3D["qg"       + histName] = new TH3D("qg"       + histName + "3D", "quark-gluon likelihood",     xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, otherBins.size()-1, otherBinsArray);
+            plots3D["cdf"      + histName] = new TH3D("cdf"      + histName + "3D", "quark-gluon CDF-likelihood", xAxisBins.size()-1, xAxisBinsArray, yAxisBins.size()-1, yAxisBinsArray, otherBins.size()-1, otherBinsArray);
           }
         }
       }
@@ -118,19 +119,21 @@ int main(int argc, char**argv){
         float qgcdf = localQG_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {(float) t.mult, t.ptD, t.axis2});
 
         TString histName = "_" + type + "_" + bins.name;
-        plots["axis2" + histName]->Fill(xAxisVar, yAxisVar, t.axis2, 	t.weight);
-        plots["ptD"   + histName]->Fill(xAxisVar, yAxisVar, t.ptD, 	t.weight);
-        plots["mult"  + histName]->Fill(xAxisVar, yAxisVar, t.mult, 	t.weight);
-        plots["nChg"  + histName]->Fill(xAxisVar, yAxisVar, t.nChg, 	t.weight);
-        plots["qg"    + histName]->Fill(xAxisVar, yAxisVar, qg, 	t.weight);
-        plots["cdf"   + histName]->Fill(xAxisVar, yAxisVar, qgcdf, 	t.weight);
+        plots["axis2"    + histName]->Fill(xAxisVar, yAxisVar, t.axis2,    t.weight);
+        plots["ptD"      + histName]->Fill(xAxisVar, yAxisVar, t.ptD,      t.weight);
+        plots["mult"     + histName]->Fill(xAxisVar, yAxisVar, t.mult, 	   t.weight);
+        plots["nChg"     + histName]->Fill(xAxisVar, yAxisVar, t.nChg, 	   t.weight);
+        plots["nNeutral" + histName]->Fill(xAxisVar, yAxisVar, t.nNeutral, t.weight);
+        plots["qg"       + histName]->Fill(xAxisVar, yAxisVar, qg,         t.weight);
+        plots["cdf"      + histName]->Fill(xAxisVar, yAxisVar, qgcdf,      t.weight);
         if(extraBinInformation){
-          plots3D["axis2" + histName]->Fill(xAxisVar, yAxisVar, t.axis2, 	t.weight);
-          plots3D["ptD"   + histName]->Fill(xAxisVar, yAxisVar, t.ptD, 		t.weight);
-          plots3D["mult"  + histName]->Fill(xAxisVar, yAxisVar, t.mult, 	t.weight);
-          plots3D["nChg"  + histName]->Fill(xAxisVar, yAxisVar, t.nChg, 	t.weight);
-          plots3D["qg"    + histName]->Fill(xAxisVar, yAxisVar, qg, 		t.weight);
-          plots3D["cdf"   + histName]->Fill(xAxisVar, yAxisVar, qgcdf, 		t.weight);
+          plots3D["axis2"    + histName]->Fill(xAxisVar, yAxisVar, t.axis2,    t.weight);
+          plots3D["ptD"      + histName]->Fill(xAxisVar, yAxisVar, t.ptD,      t.weight);
+          plots3D["mult"     + histName]->Fill(xAxisVar, yAxisVar, t.mult,     t.weight);
+          plots3D["nChg"     + histName]->Fill(xAxisVar, yAxisVar, t.nChg,     t.weight);
+          plots3D["nNeutral" + histName]->Fill(xAxisVar, yAxisVar, t.nNeutral, t.weight);
+          plots3D["qg"       + histName]->Fill(xAxisVar, yAxisVar, qg, 	      t.weight);
+          plots3D["cdf"      + histName]->Fill(xAxisVar, yAxisVar, qgcdf,     t.weight);
         }
       }
 
@@ -219,5 +222,6 @@ int main(int argc, char**argv){
       for(auto& plot : plots3D) delete plot.second;
     }
   }
-  return 0;
+ }
+ return 0;
 }

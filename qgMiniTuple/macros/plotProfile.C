69d11655b1c3e114d99f07244af2a20be2c5816d
UAEDF-tomc <tom.cornelis@ua.ac.be>
2014-12-29 17:48:58 +0100

    Bring up to date and clean-up

--- qgMiniTuple/macros/plotProfile.C
+++ qgMiniTuple/macros/plotProfile.C
@@ -12,39 +12,46 @@
 #include "TLegend.h"
 #include "TLatex.h"
 #include "binClass.h"
-#include "treeLooper2.h"
+#include "binningConfigurations.h"
+#include "treeLooper.h"
 #include "../localQGLikelihoodCalculator/localQGLikelihoodCalculator.h"
 #include "../localQGLikelihoodCalculator/localQGLikelihoodCalculator.cc"
 
 
+void setCorrectMinimum(TProfile* h){
+  bool first = true;
+  float minimum = 0;
+  for(int i = 0; i < h->GetSize(); ++i){
+    if(h->GetBinContent(i) == 0) continue;
+    if(first || h->GetBinContent(i) < minimum){
+      first = false;
+      minimum = h->GetBinContent(i);
+    }
+  }
+  h->SetMinimum(minimum);
+}
+
+
 int main(int argc, char**argv){
- bool useBins = false;
- for(TString xVar : {"deltaRmin", "closebyJetsInCone"}){
+ for(bool useBins : {true}){ 
+ for(TString xVar : {"rho"}){
 
 //  std::vector<TString> files	= {"QCD_AllPtBins"};
   std::vector<TString> files	= {"QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8_S14"};
-  std::vector<TString> jetTypes = {"AK4chs"};
+//std::vector<TString> files	= {"TTJets"};
+  std::vector<TString> jetTypes = {"AK4","AK4chs"};
 
-  // Define binning for plots (i.e. separate plots for each bin)
-  binClass bins;
-  if(useBins) bins.setBinRange("eta", 	"#eta",		{0,1.3,1.5,2,2.5,3,4.7});
-  if(useBins) bins.setBinRange("pt" , 	"p_{T}",	bins.getBins(20, 20, 2000, true, {6500}));				// i.e. 20 bins from 20 to 2000 with log=true and with an additional bin up to 6500
+  binClass bins = getSmallEtaBinning();
   bins.printBinRanges();
-
-  // Link some bins to be merged because of low statistics (for example higher pT bins at large eta)
-  if(useBins){
-    for(int i=10; i < 21; ++i) bins.setLinks("eta5_pt9", {TString::Format("eta5_pt%d",i)});
-    for(int i=14; i < 21; ++i) bins.setLinks("eta4_pt13", {TString::Format("eta4_pt%d",i)});
-    for(int i=16; i < 21; ++i) bins.setLinks("eta3_pt15", {TString::Format("eta3_pt%d",i)});
-    for(int i=18; i < 21; ++i) bins.setLinks("eta2_pt17", {TString::Format("eta2_pt%d",i)});
-    for(int i=19; i < 21; ++i) bins.setLinks("eta1_pt18", {TString::Format("eta1_pt%d",i)});
-  }
 
   // Define x and y-axis variable and bins
   std::vector<float> xAxisBins = {0,1}; TString xTitle = ""; bool xLog = false;
   if(xVar == "eta"){ 	      		xAxisBins = bins.getBins(40, 0, 4, false, {4.2,4.4,4.7});		xTitle = "#eta";}
-  if(xVar == "deltaRmin"){ 		xAxisBins = bins.getBins(16, .4, .8, false, {0.85,0.9,1,1.2}); 		xTitle = "#DeltaR_{min}";}
+  if(xVar == "deltaRmin"){ 		xAxisBins = bins.getBins(6, .4, .7, false, {0.8,1});	 		xTitle = "#DeltaR_{min}";}
+//if(xVar == "deltaRmin"){ 		xAxisBins = bins.getBins(16, .4, .8, false, {0.85,0.9,1,1.2}); 		xTitle = "#DeltaR_{min}";}
   if(xVar == "closebyJetsInCone"){  	xAxisBins = bins.getBins(9, -0.5, 8.5, false, {});		 	xTitle = "N_{jets} within #DeltaR < 0.8";}
+  if(xVar == "closebyJetsInCone10GeV"){	xAxisBins = bins.getBins(9, -0.5, 8.5, false, {});		 	xTitle = "N_{jets} within #DeltaR < 0.8";}
+  if(xVar == "ratioDoubleCone"){  	xAxisBins = bins.getBins(8, 1, 2, false, {});		 		xTitle = "p_{T}/p_{T}(doubleCone)";}
   if(xVar == "rho"){  			xAxisBins = bins.getBins(9, 5, 50, false, {});		 		xTitle = "#rho";}
   if(xVar == "nPileUp"){  		xAxisBins = bins.getBins(12, 9.5, 69.5, false, {});		 	xTitle = "nPileUp";}
   if(xVar == "nPriVtxs"){  		xAxisBins = bins.getBins(9, 9.5, 54.5, false, {});		 	xTitle = "nPriVtxs";}
@@ -60,71 +67,61 @@ int main(int argc, char**argv){
       treeLooper t(file, jetType);							// Init tree
       bins.setReference("pt",  &t.pt);
       bins.setReference("eta", &t.eta);
+      bins.setReference("rho", &t.rho);
 
       // Set x-axis variable
-      float& xAxisVar = (xVar == "deltaRmin" ? 		t.closestJetdR : 
-                        (xVar == "closebyJetsInCone" ? 	t.closebyJetsInCone : 
-                        (xVar == "rho" ? 		t.rho : 
-			t.eta)));
-//      int& xAxisVar =   (xVar == "nPileUp" ? 		t.nPileUp : t.nPriVtxs);
+      float& xAxisVar = (xVar == "deltaRmin" ? 			t.closestJetdR : 
+                        (xVar == "closebyJetsInCone" ? 		t.closebyJetsInCone : 
+                        (xVar == "closebyJetsInCone10GeV" ? 	t.closebyJetsInCone10GeV : 
+                        (xVar == "ratioDoubleCone" ? 		t.ratioDoubleCone : 
+                        (xVar == "rho" ? 			t.rho : 
+			t.eta)))));
+//    int& xAxisVar =   (xVar == "nPileUp" ? 			t.nPileUp : t.nPriVtxs);
 
       // Init local QGLikelihoodCalculator
-      QGLikelihoodCalculator localQG("../data/pdfQG_" + jetType + "_13TeV.root");
-      QGLikelihoodCalculator localQG_cdf("../data/pdfQG_" + jetType + "_fineBinning_13TeV.root");
+      QGLikelihoodCalculator localQG("../data/pdfQG_" + jetType + "_13TeV_v1_newTest.root");
 
       // Creation of histos
       std::map<TString, TProfile*> plots;
       for(TString type : {"quark","gluon"}){
         for(TString binName : bins.getAllBinNames()){
           TString histName = "_" + type + "_" + binName;
-          for(TString dRcut : {"", "_dR2", "_dR3"}){
-            plots["axis2" + dRcut + histName] = new TProfile("axis2" + dRcut + histName, "mean -log(#sigma_{2})",           xAxisBins.size()-1, xAxisBinsArray);
-            plots["ptD"   + dRcut + histName] = new TProfile("ptD"   + dRcut + histName, "mean p_{T}D",                     xAxisBins.size()-1, xAxisBinsArray);
-            plots["nChg"  + dRcut + histName] = new TProfile("nChg"  + dRcut + histName, "mean multiplicity",               xAxisBins.size()-1, xAxisBinsArray);
-            plots["mult"  + dRcut + histName] = new TProfile("mult"  + dRcut + histName, "mean multiplicity",               xAxisBins.size()-1, xAxisBinsArray);
-            plots["qg"    + dRcut + histName] = new TProfile("qg"    + dRcut + histName, "mean quark-gluon likelihood",     xAxisBins.size()-1, xAxisBinsArray);
-            plots["cdf"   + dRcut + histName] = new TProfile("cdf"   + dRcut + histName, "mean quark-gluon CDF-likelihood", xAxisBins.size()-1, xAxisBinsArray);
-          }
+          plots["axis2" + histName] = new TProfile("axis2" + histName, "mean -log(#sigma_{2})",           xAxisBins.size()-1, xAxisBinsArray);
+          plots["ptD"   + histName] = new TProfile("ptD"   + histName, "mean p_{T}D",                     xAxisBins.size()-1, xAxisBinsArray);
+          plots["mult"  + histName] = new TProfile("mult"  + histName, "mean multiplicity",               xAxisBins.size()-1, xAxisBinsArray);
+          plots["qg"    + histName] = new TProfile("qg"    + histName, "mean quark-gluon likelihood",     xAxisBins.size()-1, xAxisBinsArray);
         }
       }
 
       // Fill histos
+      TString binName;
       while(t.next()){
-        if(!bins.update()) 	continue;										// Find bin and return false if outside ranges 
-        if(t.jetIdLevel < 3) 	continue;										// Select tight jets
-        if(!t.matchedJet) 	continue; 										// Only matched jets
-        if(t.bTag) 		continue;										// Anti-b tagging
-//	if(!t.balanced) 	continue;										// Take only two leading jets with pt3 < 0.15*(pt1+pt2)
-//      if(t.nGenJetsInCone != 1 || t.nJetsForGenParticle != 1 || t.nGenJetsForGenParticle != 1) continue;		// Use only jets matched to exactly one gen jet and gen particle, and no other jet candidates
+        if(!bins.getBinName(binName)) 	continue;									// Find bin and return false if outside ranges 
+        if(t.jetIdLevel < 3) 		continue;									// Select tight jets
+        if(!t.matchedJet) 		continue; 									// Only matched jets
+        if(t.bTag) 			continue;									// Anti-b tagging
+	if(!t.balanced) 		continue;									// Take only two leading jets with pt3 < 0.15*(pt1+pt2)
+        if(t.nGenJetsInCone != 1 || t.nJetsForGenParticle != 1 || t.nGenJetsForGenParticle != 1) continue;		// Use only jets matched to exactly one gen jet and gen particle, and no other jet candidates
 
         TString type;
         if(t.partonId == 21) 	 	type = "gluon";
         else if(fabs(t.partonId) < 4) 	type = "quark";
         else continue;
 
-        for(TString dRcut : {"","_dR2","_dR3"}){
-          int& mult    = (dRcut == ""? t.mult  : (dRcut == "_dR2"? t.mult_dR2  : t.mult_dR3));
-          int& nChg    = (dRcut == ""? t.nChg  : (dRcut == "_dR2"? t.nChg_dR2  : t.nChg_dR3));
-          float& ptD   = (dRcut == ""? t.ptD   : (dRcut == "_dR2"? t.ptD_dR2   : t.ptD_dR3));
-          float& axis2 = (dRcut == ""? t.axis2 : (dRcut == "_dR2"? t.axis2_dR2 : t.axis2_dR3));
-          if(mult < 3 || axis2 > 99) continue; 										// Take jets with at least 3 particles
+        if(t.mult < 3 || t.axis2 > 99) continue; 									// Take jets with at least 3 particles
 
-          float qg = localQG.computeQGLikelihood(          t.pt, t.eta, t.rho, {(float)mult, ptD, axis2});
-          float qgcdf = localQG_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {(float)mult, ptD, axis2});
+        float qg = localQG.computeQGLikelihood(          t.pt, t.eta, t.rho, {(float)t.mult, t.ptD, t.axis2});
 
-          TString histName = "_" + type + "_" + bins.name;
-          plots["axis2" + dRcut + histName]->Fill(xAxisVar, axis2, 	t.weight);
-          plots["ptD"   + dRcut + histName]->Fill(xAxisVar, ptD, 	t.weight);
-          plots["nChg"  + dRcut + histName]->Fill(xAxisVar, nChg, 	t.weight);
-          plots["mult"  + dRcut + histName]->Fill(xAxisVar, mult, 	t.weight);
-          plots["qg"    + dRcut + histName]->Fill(xAxisVar, qg, 	t.weight);
-          plots["cdf"   + dRcut + histName]->Fill(xAxisVar, qgcdf, 	t.weight);
-        }
+        TString histName = "_" + type + "_" + binName;
+        plots["axis2" + histName]->Fill(xAxisVar, t.axis2, 	t.weight);
+        plots["ptD"   + histName]->Fill(xAxisVar, t.ptD, 	t.weight);
+        plots["mult"  + histName]->Fill(xAxisVar, t.mult, 	t.weight);
+        plots["qg"    + histName]->Fill(xAxisVar, qg, 		t.weight);
       }
 
       // Stacking, cosmetics and saving
       for(auto& plot : plots){
-        if(!plot.first.Contains("gluon") || !plot.first.Contains("dR3")) continue;
+        if(!plot.first.Contains("gluon")) continue;
         TCanvas c;
         if(xLog) c.SetLogx();
 
@@ -135,13 +132,11 @@ int main(int argc, char**argv){
        
         float maximum = 0, minimum = 99999; 
         for(TString type : {"quark", "gluon"}){
-          for(TString dRcut : {"", "_dR2", "_dR3"}){
-            TString histName = plot.first;
-            histName.ReplaceAll("gluon", type);
-            histName.ReplaceAll("_dR3", dRcut);
-            if(maximum < plots[histName]->GetMaximum()) maximum = plots[histName]->GetMaximum();
-            if(plots[histName]->GetMinimum() > 0.01 && minimum > plots[histName]->GetMinimum()) minimum = plots[histName]->GetMinimum();
-          }
+          TString histName = plot.first;
+          histName.ReplaceAll("gluon", type);
+          setCorrectMinimum(plots[histName]);
+          if(maximum < plots[histName]->GetMaximum()) maximum = plots[histName]->GetMaximum();
+          if(minimum > plots[histName]->GetMinimum()) minimum = plots[histName]->GetMinimum();
         }
 
         plot.second->GetXaxis()->SetTitle(xTitle);
@@ -153,18 +148,15 @@ int main(int argc, char**argv){
         plot.second->Draw("HIST LP");
         std::vector<int> color = {2,46,3,30,4,39};
         int i = 0;
-        for(TString dRcut : {"", "_dR2", "_dR3"}){
-          for(TString type : {"quark", "gluon"}){
-            TString histName = plot.first;
-            histName.ReplaceAll("gluon", type);
-            histName.ReplaceAll("_dR3", dRcut);
-            plots[histName]->SetMarkerStyle(20);
-            plots[histName]->SetMarkerSize(0.5);
-            plots[histName]->SetMarkerColor(color[i]);
-            plots[histName]->SetLineColor(color[i++]);
-            plots[histName]->Draw("HIST LP same");
-            l.AddEntry(plots[histName], type + "s" + (dRcut == "_dR3" ? " (#DeltaR < 0.3)" : (dRcut == "_dR2" ? " (#DeltaR < 0.2)" : "")), "p");
-          }
+        for(TString type : {"quark", "gluon"}){
+          TString histName = plot.first;
+          histName.ReplaceAll("gluon", type);
+          plots[histName]->SetMarkerStyle(20);
+          plots[histName]->SetMarkerSize(0.5);
+          plots[histName]->SetMarkerColor(color[i]);
+          plots[histName]->SetLineColor(color[i++]);
+          plots[histName]->Draw("HIST LP same");
+          l.AddEntry(plots[histName], type + "s", "p");
         }
         l.Draw();
 
@@ -173,7 +165,7 @@ int main(int argc, char**argv){
         TString variable = plot.first(0, plot.first.First("_"));
         TString pdfDir = "./plots/profile/" + file + "/" + jetType + "/" + xVar + (useBins ? "" : "_nobins") + "/" + variable + "/";
         TString pdfName = pdfDir + plot.first + ".pdf";
-        pdfName.ReplaceAll("_gluon","").ReplaceAll("_dR3","");
+        pdfName.ReplaceAll("_gluon","");
         system("mkdir -p " + pdfDir);
         c.SaveAs(pdfName);
       }
@@ -181,6 +173,7 @@ int main(int argc, char**argv){
       for(auto& plot : plots) delete plot.second;
     }
   }
+ }
  }
  return 0;
 }

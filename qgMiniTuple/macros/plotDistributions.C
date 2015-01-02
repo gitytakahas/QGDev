d16d8187682272ee2e225d889b035dff4b87fa8a
UAEDF-tomc <tom.cornelis@ua.ac.be>
2015-01-02 18:40:33 +0100

    Bring plotDistributions macro up to date

--- qgMiniTuple/macros/plotDistributions.C
+++ qgMiniTuple/macros/plotDistributions.C
@@ -11,8 +11,8 @@
 #include "TLegend.h"
 #include "TVector.h"
 #include "binClass.h"
-#include "treeLooper2.h"
-#include "getTransform.h"
+#include "binningConfigurations.h"
+#include "treeLooper.h"
 #include "../localQGLikelihoodCalculator/localQGLikelihoodCalculator.h"
 #include "../localQGLikelihoodCalculator/localQGLikelihoodCalculator.cc"
 
@@ -23,36 +23,21 @@ int main(int argc, char**argv){
   bool useDecorrelation		= false;
 
   // Define binning for plots
-  binClass bins;
-  bins.setBinRange("eta",	"#eta",		{0,1.3,1.5,2,2.5,3,4.7});
-  bins.setBinRange("pt" , 	"p_{T}",	bins.getBins(20, 20, 2000, true, {6500}));				// i.e. 20 bins from 20 to 2000 with log=true and with an additional bin up to 6500
-//  bins.setBinRange("cbjdR", {0, 0.55, 0.65,0.75,1,2,9999});
-  bins.printBinRanges();
-
-  // Link some bins to be merged because of low statistics (for example higher pT bins at large eta)
-  for(int i=10; i < 21; ++i) bins.setLinks("eta5_pt9", {TString::Format("eta5_pt%d",i)});
-  for(int i=14; i < 21; ++i) bins.setLinks("eta4_pt13", {TString::Format("eta4_pt%d",i)});
-  for(int i=16; i < 21; ++i) bins.setLinks("eta3_pt15", {TString::Format("eta3_pt%d",i)});
-  for(int i=18; i < 21; ++i) bins.setLinks("eta2_pt17", {TString::Format("eta2_pt%d",i)});
-  for(int i=19; i < 21; ++i) bins.setLinks("eta1_pt18", {TString::Format("eta1_pt%d",i)});
+  binClass bins = getV1Binning();
 
   // For different jet types (if _antib is added bTag is applied)
   for(TString file : {"QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8_S14"}){
-    for(TString jetType : {"AK4chs"}){//,"AK5","AK5chs","AK7","AK7chs"}){
+    for(TString jetType : {"AK4","AK4chs"}){
       std::cout << "Making plots for " << jetType << " in file " << file << "..." << std::endl;
       system("rm -rf plots/distributions/" + file + "/" + jetType);
 
       treeLooper t(file, jetType);						// Init tree
       bins.setReference("pt",  &t.pt);
       bins.setReference("eta", &t.eta);
-//      bins.setReference("cbjdR", &t.closestJetdR);
+      bins.setReference("rho", &t.rho);
 
       // Init local localQGikelihoodCalculator
-      QGLikelihoodCalculator localQG(    "../data/pdfQG_" + jetType + "_13TeV.root");
-      QGLikelihoodCalculator localQG_cdf("../data/pdfQG_" + jetType + "_fineBinning_13TeV.root");
-
-      std::map<TString, std::vector<std::vector<double>>> decorrelationMatrices;
-      if(useDecorrelation && !getMatricesFromFile(decorrelationMatrices, "../data/pdfQG_" + jetType + "_13TeV.root")) exit(1);
+      QGLikelihoodCalculator localQG(    "../data/pdfQG_" + jetType + "_13TeV_v1g_newTest.root");
 
       // Creation of histos
       std::map<TString, TH1D*> plots;
@@ -63,43 +48,30 @@ int main(int argc, char**argv){
           plots["axis2"  + histName] 	= new TH1D("axis2" + histName, "axis2" + histName, 100, 1, 9);
           plots["ptD"    + histName]	= new TH1D("ptD"   + histName, "ptD"   + histName, 100, 0, 1);
           plots["mult"   + histName]	= new TH1D("mult"  + histName, "mult"  + histName, 100, 0.5, 100.5);
-          plots["axis2_dR2"  + histName] 	= new TH1D("axis2_dR2" + histName, "axis2" + histName, 100, 1, 9);
-          plots["ptD_dR2"    + histName]	= new TH1D("ptD_dR2"   + histName, "ptD"   + histName, 100, 0, 1);
-          plots["mult_dR2"   + histName]	= new TH1D("mult_dR2"  + histName, "mult"  + histName, 100, 0.5, 100.5);
-          plots["axis2_dR3"  + histName] 	= new TH1D("axis2_dR3" + histName, "axis2" + histName, 100, 1, 9);
-          plots["ptD_dR3"    + histName]	= new TH1D("ptD_dR3"   + histName, "ptD"   + histName, 100, 0, 1);
-          plots["mult_dR3"   + histName]	= new TH1D("mult_dR3"  + histName, "mult"  + histName, 100, 0.5, 100.5);
           for(TString var : {"qg","axis2","ptD","mult"}){
-            for(TString type : {"_l","_cdf"}) plots[var + type + histName] = new TH1D(var + type + histName, var + type + histName, 100, -0.0001, 1.0001);
-          }
-          if(useDecorrelation){
-            auto varRanges = calcRangeTransformation(decorrelationMatrices[binName], {0, 0.5, 0}, {8, 100.5, 1});
-            plots["var1" + histName] = new TH1D("var1" + histName, "var1" + histName, 100, varRanges[0][0], varRanges[1][0]);
-            plots["var2" + histName] = new TH1D("var2" + histName, "var2" + histName, 100, varRanges[0][1], varRanges[1][1]);
-            plots["var3" + histName] = new TH1D("var3" + histName, "var3" + histName, 100, varRanges[0][2], varRanges[1][2]);
+            plots[var + "_l" + histName] = new TH1D(var + "_l" + histName, var + "_l" + histName, 100, -0.0001, 1.0001);
           }
           if(plot2D){
-            for(TString var2D : {"qg-axis2","qg-ptD","qg-mult","axis2-ptD","axis2-mult","ptD-mult","ptD-dR2"}){
+            for(TString var2D : {"qg-axis2","qg-ptD","qg-mult","axis2-ptD","axis2-mult","ptD-mult"}){
               plots2D[var2D + histName] = new TH2D(var2D + histName, var2D + histName, 100, -0.0001, 1.0001, 100, -0.0001,1.001);
             }
-            plots2D["axis2-dR2" + histName] = new TH2D("axis2-dR2" + histName, "axis2-dR2" + histName, 100, 1, 9, 100, 1, 9);
-            plots2D["mult-dR2" + histName] = new TH2D("mult-dR2" + histName, "mult-dR2" + histName, 100, 0.5, 100.5, 100, 0.5, 100.5);
           }
         }
       }
 
       // Fill histos
+      TString binName;
       while(t.next()){
-        if(!bins.update()) 	continue;										// Find bin and return false if outside ranges 
-        if(t.jetIdLevel < 3) 	continue;										// Select tight jets
-        if(!t.matchedJet) 	continue; 										// Only matched jets
-        if(t.bTag) 		continue;										// Anti-b tagging
-        if(t.mult < 3) 		continue; 										// Need at least three particles in the jet
+        if(!bins.getBinName(binName)) 	continue;										// Find bin and return false if outside ranges 
+        if(t.jetIdLevel < 3) 		continue;										// Select tight jets
+        if(!t.matchedJet) 		continue; 										// Only matched jets
+        if(t.bTag) 			continue;										// Anti-b tagging
+        if(t.mult < 3) 			continue; 										// Need at least three particles in the jet
 
         TString type;
         if(t.nGenJetsInCone < 1)		type = "pu";
         else if(!t.matchedJet)			type = "undefined";
-//        else if(!t.balanced)			type = "undefined";
+        else if(!t.balanced)			type = "undefined";
         else if(t.nGenJetsInCone > 1)		type = "undefined";
         else if(t.nJetsForGenParticle != 1)	type = "undefined";
         else if(t.nGenJetsForGenParticle != 1)	type = "undefined";
@@ -109,47 +81,27 @@ int main(int argc, char**argv){
         else if(fabs(t.partonId) == 5) 		type = "bquark";
         else 					type = "undefined";
 
-        TString histName = "_" + type + "_" + bins.name; 
-        plots["axis2"   + histName]->Fill(t.axis2, 											t.weight);
-        plots["axis2_dR2"   + histName]->Fill(t.axis2_dR2, 										t.weight);
-        plots["axis2_dR3"   + histName]->Fill(t.axis2_dR3, 										t.weight);
-        plots["ptD"     + histName]->Fill(t.ptD, 											t.weight);
-        plots["ptD_dR2"     + histName]->Fill(t.ptD_dR2, 										t.weight);
-        plots["ptD_dR3"     + histName]->Fill(t.ptD_dR3, 										t.weight);
-        plots["mult"    + histName]->Fill(t.mult, 											t.weight);
-        plots["mult_dR2"    + histName]->Fill(t.mult_dR2, 										t.weight);
-        plots["mult_dR3"    + histName]->Fill(t.mult_dR3, 										t.weight);
-        plots["qg_l"    + histName]->Fill(localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {(float) t.mult, t.ptD, t.axis2}),	t.weight);
-        plots["axis2_l" + histName]->Fill(localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {-1, -1, t.axis2}), 			t.weight);
-        plots["ptD_l"   + histName]->Fill(localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {-1, t.ptD}), 				t.weight);
-        plots["mult_l"  + histName]->Fill(localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {(float) t.mult}), 			t.weight);
-        plots["qg_cdf"    + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {(float) t.mult, t.ptD, t.axis2}), 	t.weight);
-        plots["axis2_cdf" + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {-1, -1, t.axis2}), 			t.weight);
-        plots["ptD_cdf"   + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {-1, t.ptD}), 			t.weight);
-        plots["mult_cdf"  + histName]->Fill(localQG_cdf.computeQGLikelihoodCDF(t.pt, t.eta, t.rho, {(float) t.mult}), 			t.weight);
+        float qg_l      = localQG.computeQGLikelihood(t.pt, t.eta, t.rho, {(float) t.mult, t.ptD, t.axis2});
+        float axis2_l   = localQG.computeQGLikelihood(t.pt, t.eta, t.rho, {-1, -1, t.axis2});
+        float ptD_l     = localQG.computeQGLikelihood(t.pt, t.eta, t.rho, {-1, t.ptD});
+        float mult_l    = localQG.computeQGLikelihood(t.pt, t.eta, t.rho, {(float) t.mult});
+
+        TString histName = "_" + type + "_" + binName; 
+        plots["axis2"   + histName]->Fill(t.axis2, 	t.weight);
+        plots["ptD"     + histName]->Fill(t.ptD, 	t.weight);
+        plots["mult"    + histName]->Fill(t.mult, 	t.weight);
+        plots["qg_l"    + histName]->Fill(qg_l,		t.weight);
+        plots["axis2_l" + histName]->Fill(axis2_l,	t.weight);
+        plots["ptD_l"   + histName]->Fill(ptD_l,	t.weight);
+        plots["mult_l"  + histName]->Fill(mult_l,	t.weight);
 
         if(plot2D){
-          float qg_l 	= localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {(float) t.mult, t.ptD, t.axis2});
-          float axis2_l = localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {-1, -1, t.axis2});
-          float ptD_l 	= localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {-1, t.ptD});
-          float mult_l 	= localQG.computeQGLikelihood(       t.pt, t.eta, t.rho, {(float) t.mult});
           plots2D["qg-axis2"   + histName]->Fill(qg_l, 		axis2_l, t.weight);
           plots2D["qg-ptD"     + histName]->Fill(qg_l, 		ptD_l, 	 t.weight);
           plots2D["qg-mult"    + histName]->Fill(qg_l, 		mult_l,  t.weight);
           plots2D["axis2-ptD"  + histName]->Fill(axis2_l, 	ptD_l, 	 t.weight);
           plots2D["axis2-mult" + histName]->Fill(axis2_l, 	mult_l,  t.weight);
           plots2D["ptD-mult"   + histName]->Fill(ptD_l, 	mult_l,  t.weight);
-          plots2D["axis2-dR2"  + histName]->Fill(t.axis2, 	t.axis2_dR2,  	t.weight);
-          plots2D["mult-dR2"   + histName]->Fill(t.mult, 	t.mult_dR2,  	t.weight);
-          plots2D["ptD-dR2"    + histName]->Fill(t.ptD, 	t.ptD_dR2,  	t.weight);
-        }
-
-        if(useDecorrelation){
-          std::vector<double> vars = {t.axis2, (double) t.mult, t.ptD};
-          std::vector<double> uncorrVars = decorrelate(decorrelationMatrices[bins.name], vars); 
-          plots["var1" + histName]->Fill(uncorrVars[0]);
-          plots["var2" + histName]->Fill(uncorrVars[1]);
-          plots["var3" + histName]->Fill(uncorrVars[2]);
         }
       }
       if(norm) for(auto& plot : plots) plot.second->Scale(1./plot.second->Integral(0, plot.second->GetNbinsX() + 1));
@@ -169,7 +121,6 @@ int main(int argc, char**argv){
         if(plot.first.Contains("mult"))  axisTitle = "multiplicity";
         if(plot.first.Contains("qg"))    axisTitle = "quark-gluon";
         if(plot.first.Contains("_l"))	 axisTitle += " likelihood";
-        if(plot.first.Contains("_cdf"))	 axisTitle += " CDF-likelihood";
 
         THStack stack(plot.first,";"+axisTitle+";"+(norm?"fraction of ":"")+"jets/bin");
         double maximum = 0;

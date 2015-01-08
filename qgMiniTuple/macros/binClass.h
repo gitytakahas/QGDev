093787deaf9bda97aff62e5f2f970a17037bea83
UAEDF-tomc <tom.cornelis@ua.ac.be>
2015-01-08 16:44:03 +0100

    Make getWeight method a bit more safe

--- qgMiniTuple/macros/binClass.h
+++ qgMiniTuple/macros/binClass.h
@@ -44,7 +44,7 @@ class binClass{
     void 			setLink(TString, TString);
     void 			setReference(TString name, float* pointer){ 		 varPointers[name] = pointer;}
     void 			setWeights(TString binName, std::vector<float> weights){ varWeights[binName] = weights;}
-    float 			getWeight(TString binName, int varIndex){ return varWeights[binName][varIndex];}
+    float 			getWeight(TString binName, int varIndex){ 		 return varWeights.count(binName)? varWeights[binName][varIndex] : 1.;};
 
     // Functions to retrieve all bin names, and the links between them 
     std::vector<TString> 	getAllBinNames(bool);

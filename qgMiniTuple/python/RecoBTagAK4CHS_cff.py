829c8105211c821f99d1ced33eb91ed252ded78c
UAEDF-tomc <tom.cornelis@ua.ac.be>
2015-01-09 16:33:41 +0100

    Simplify b-tagging sequence and use new CSVv2+IVF algorithm

--- qgMiniTuple/python/RecoBTagAK4CHS_cff.py
+++ qgMiniTuple/python/RecoBTagAK4CHS_cff.py
@@ -1,40 +1,19 @@
 import FWCore.ParameterSet.Config as cms
-from RecoJets.JetAssociationProducers.ak5JTA_cff import *
+from RecoJets.JetAssociationProducers.ak4JTA_cff import *
 from RecoBTag.Configuration.RecoBTag_cff import *
 
-ak4CHSJetTracksAssociatorAtVertex 		= ak5JetTracksAssociatorAtVertexPF.clone(jets = "ak4PFJetsCHS")
-ak4CHSImpactParameterTagInfos 			= impactParameterTagInfos.clone(jetTracks = 'ak4CHSJetTracksAssociatorAtVertex')
-ak4CHSTrackCountingHighEffBJetTags 		= trackCountingHighEffBJetTags.clone(tagInfos = cms.VInputTag('ak4CHSImpactParameterTagInfos'))
-ak4CHSTrackCountingHighPurBJetTags 		= trackCountingHighPurBJetTags.clone(tagInfos = cms.VInputTag('ak4CHSImpactParameterTagInfos'))
-ak4CHSJetProbabilityBJetTags 			= jetProbabilityBJetTags.clone(tagInfos = cms.VInputTag('ak4CHSImpactParameterTagInfos'))
-ak4CHSJetBProbabilityBJetTags 			= jetBProbabilityBJetTags.clone(tagInfos = cms.VInputTag('ak4CHSImpactParameterTagInfos'))
+ak4CHSJetTracksAssociator                    = ak4JetTracksAssociatorAtVertexPF.clone(jets = "ak4PFJetsCHS")
+ak4CHSImpactParameterTagInfos                = impactParameterTagInfos.clone(jetTracks = 'ak4CHSJetTracksAssociator')
+ak4CHSInclusiveSecondaryVertexFinderTagInfos = inclusiveSecondaryVertexFinderTagInfos.clone(trackIPTagInfos = 'ak4CHSImpactParameterTagInfos')
 
-ak4CHSSecondaryVertexTagInfos 			= secondaryVertexTagInfos.clone(trackIPTagInfos = 'ak4CHSImpactParameterTagInfos')
-ak4CHSSimpleSecondaryVertexBJetTags 		= simpleSecondaryVertexBJetTags.clone(tagInfos = cms.VInputTag('ak4CHSSecondaryVertexTagInfos'))
-ak4CHSCombinedSecondaryVertexBJetTags 		= combinedSecondaryVertexBJetTags.clone(tagInfos = cms.VInputTag('ak4CHSImpactParameterTagInfos', 'ak4CHSSecondaryVertexTagInfos'))
-ak4CHSCombinedSecondaryVertexMVABJetTags 	= combinedSecondaryVertexMVABJetTags.clone(tagInfos = cms.VInputTag('ak4CHSImpactParameterTagInfos', 'ak4CHSSecondaryVertexTagInfos'))
-
-ak4CHSJetBtaggingIP = cms.Sequence(
-    ak4CHSImpactParameterTagInfos * (
-        ak4CHSTrackCountingHighEffBJetTags +
-        ak4CHSTrackCountingHighPurBJetTags +
-        ak4CHSJetProbabilityBJetTags +
-        ak4CHSJetBProbabilityBJetTags
-    )
+ak4CHSCombinedInclusiveSecondaryVertexV2BJetTags = combinedInclusiveSecondaryVertexV2BJetTags.clone(
+    tagInfos = cms.VInputTag(cms.InputTag("ak4CHSImpactParameterTagInfos"),
+			     cms.InputTag("ak4CHSInclusiveSecondaryVertexFinderTagInfos"))
 )
 
-ak4CHSJetBtaggingSV = cms.Sequence(
-    ak4CHSImpactParameterTagInfos *
-    ak4CHSSecondaryVertexTagInfos * (
-        ak4CHSSimpleSecondaryVertexBJetTags +
-        ak4CHSCombinedSecondaryVertexBJetTags +
-        ak4CHSCombinedSecondaryVertexMVABJetTags
-    )
-)
 
-ak4CHSJetBtagging = cms.Sequence(
-    ak4CHSJetBtaggingIP +
-    ak4CHSJetBtaggingSV
+ak4CHSBTagging = cms.Sequence(ak4CHSJetTracksAssociator *
+                              ak4CHSImpactParameterTagInfos *
+                              ak4CHSInclusiveSecondaryVertexFinderTagInfos *
+                              ak4CHSCombinedInclusiveSecondaryVertexV2BJetTags
 )
-
-ak4CHSBTagging = cms.Sequence(ak4CHSJetTracksAssociatorAtVertex * ak4CHSJetBtagging)
--- qgMiniTuple/python/RecoBTagAK4_cff.py
+++ qgMiniTuple/python/RecoBTagAK4_cff.py
@@ -1,40 +1,19 @@
 import FWCore.ParameterSet.Config as cms
-from RecoJets.JetAssociationProducers.ak5JTA_cff import *
+from RecoJets.JetAssociationProducers.ak4JTA_cff import *
 from RecoBTag.Configuration.RecoBTag_cff import *
 
-ak4JetTracksAssociatorAtVertex 		= ak5JetTracksAssociatorAtVertexPF.clone(jets = "ak4PFJets")
-ak4ImpactParameterTagInfos 		= impactParameterTagInfos.clone(jetTracks = 'ak4JetTracksAssociatorAtVertex')
-ak4TrackCountingHighEffBJetTags 	= trackCountingHighEffBJetTags.clone(tagInfos = cms.VInputTag('ak4ImpactParameterTagInfos'))
-ak4TrackCountingHighPurBJetTags 	= trackCountingHighPurBJetTags.clone(tagInfos = cms.VInputTag('ak4ImpactParameterTagInfos'))
-ak4JetProbabilityBJetTags 		= jetProbabilityBJetTags.clone(tagInfos = cms.VInputTag('ak4ImpactParameterTagInfos'))
-ak4JetBProbabilityBJetTags 		= jetBProbabilityBJetTags.clone(tagInfos = cms.VInputTag('ak4ImpactParameterTagInfos'))
+ak4JetTracksAssociator                    = ak4JetTracksAssociatorAtVertexPF.clone(jets = "ak4PFJets")
+ak4ImpactParameterTagInfos                = impactParameterTagInfos.clone(jetTracks = 'ak4JetTracksAssociator')
+ak4InclusiveSecondaryVertexFinderTagInfos = inclusiveSecondaryVertexFinderTagInfos.clone(trackIPTagInfos = 'ak4ImpactParameterTagInfos')
 
-ak4SecondaryVertexTagInfos 		= secondaryVertexTagInfos.clone(trackIPTagInfos = 'ak4ImpactParameterTagInfos')
-ak4SimpleSecondaryVertexBJetTags 	= simpleSecondaryVertexBJetTags.clone(tagInfos = cms.VInputTag('ak4SecondaryVertexTagInfos'))
-ak4CombinedSecondaryVertexBJetTags 	= combinedSecondaryVertexBJetTags.clone(tagInfos = cms.VInputTag('ak4ImpactParameterTagInfos', 'ak4SecondaryVertexTagInfos'))
-ak4CombinedSecondaryVertexMVABJetTags 	= combinedSecondaryVertexMVABJetTags.clone(tagInfos = cms.VInputTag('ak4ImpactParameterTagInfos', 'ak4SecondaryVertexTagInfos'))
-
-ak4JetBtaggingIP = cms.Sequence(
-    ak4ImpactParameterTagInfos * (
-        ak4TrackCountingHighEffBJetTags +
-        ak4TrackCountingHighPurBJetTags +
-        ak4JetProbabilityBJetTags +
-        ak4JetBProbabilityBJetTags
-    )
+ak4CombinedInclusiveSecondaryVertexV2BJetTags = combinedInclusiveSecondaryVertexV2BJetTags.clone(
+    tagInfos = cms.VInputTag(cms.InputTag("ak4ImpactParameterTagInfos"),
+			     cms.InputTag("ak4InclusiveSecondaryVertexFinderTagInfos"))
 )
 
-ak4JetBtaggingSV = cms.Sequence(
-    ak4ImpactParameterTagInfos *
-    ak4SecondaryVertexTagInfos * (
-        ak4SimpleSecondaryVertexBJetTags +
-        ak4CombinedSecondaryVertexBJetTags +
-        ak4CombinedSecondaryVertexMVABJetTags
-    )
-)
 
-ak4JetBtagging = cms.Sequence(
-    ak4JetBtaggingIP +
-    ak4JetBtaggingSV
+ak4BTagging = cms.Sequence(ak4JetTracksAssociator * 
+                           ak4ImpactParameterTagInfos *
+                           ak4InclusiveSecondaryVertexFinderTagInfos *
+                           ak4CombinedInclusiveSecondaryVertexV2BJetTags
 )
-
-ak4BTagging = cms.Sequence(ak4JetTracksAssociatorAtVertex * ak4JetBtagging)

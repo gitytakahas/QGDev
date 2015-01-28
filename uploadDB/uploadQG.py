8a6c004f9bd30aa9aaeeff9b32f7acca7b3512e7
UAEDF-tomc <tom.cornelis@ua.ac.be>
2015-01-28 12:16:45 +0100

    Uploading to correct database now

--- uploadDB/uploadQG.py
+++ uploadDB/uploadQG.py
@@ -8,7 +8,7 @@ from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option('-f', '--file', type='string', action='store', dest='file', help='the database file to be uploaded')
-parser.add_option('-o', '--offline', action='store_true', dest='offline', default=False, help='Upload to the offline DB instead of PREP')
+parser.add_option('-o', '--online', action='store_true', dest='online', default=False, help='Upload to the online DB instead of PREP')
 (options, args) = parser.parse_args()
 
 # Check if input file exists"
@@ -17,8 +17,8 @@ if not os.path.isfile(options.file):
   exit(1) 
 
 # Choose the database
-if options.offline: database = 'oracle://cms_orcon_prod/CMS_COND_31X_PHYSICSTOOLS'
-else:               database = 'oracle://cms_orcoff_prep/CMS_COND_TEMP'
+if options.online: database = 'oracle://cms_orcon_prod/CMS_COND_PAT_000'
+else:              database = 'oracle://cms_orcoff_prep/CMS_COND_TEMP'
 print "Uploading to database: " + database
 print
 
@@ -41,8 +41,8 @@ uploadScript = os.path.abspath('upload.py')
 for tag in tags:
   shutil.copy(options.file, tag + '.db')
   try:
-    if options.offline: print subprocess.check_output( [uploadScript, tag + '.db', '--backend', 'offline'], stderr=subprocess.STDOUT)
-    else:               print subprocess.check_output( [uploadScript, tag + '.db'], stderr=subprocess.STDOUT)
+    if options.online: print subprocess.check_output( [uploadScript, tag + '.db'], stderr=subprocess.STDOUT)
+    else:              print subprocess.check_output( [uploadScript, tag + '.db', '--backend', 'offline'], stderr=subprocess.STDOUT)
   except subprocess.CalledProcessError as e:
     print e.output
 
